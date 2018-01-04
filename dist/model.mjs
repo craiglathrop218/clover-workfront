8749a5cec270e6f189e7e1ee0d78eaf13706e185
Jaanek Oja <jaanek@oja.me>
2018-01-04 16:03:51 +0200

    Added .mjs files

---
+++ dist/model.mjs
@@ -0,0 +1,28 @@
+export var WfModel;
+(function (WfModel) {
+    ;
+    ;
+    ;
+    // This is just a meta data entity to hold document folder parent entity field name and value
+    class DocumentFolderParentField {
+        constructor(name, value) {
+            this.name = name;
+            this.value = value;
+        }
+    }
+    WfModel.DocumentFolderParentField = DocumentFolderParentField;
+    ;
+    class Issue {
+    }
+    Issue.EXT_MSG_PREFIX = "FROM_EMAIL.MESSAGE_ID:";
+    WfModel.Issue = Issue;
+    class ReplyMessage {
+        constructor() {
+            this.isReply = false;
+        }
+    }
+    WfModel.ReplyMessage = ReplyMessage;
+    class IssueUpdate {
+    }
+    WfModel.IssueUpdate = IssueUpdate;
+})(WfModel || (WfModel = {}));
--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,36 @@
 var gulp = require("gulp");
 var merge = require('merge2');  // Requires separate installation
 var ts = require("gulp-typescript");
+var rename = require('gulp-rename');
 var project = ts.createProject("tsconfig.json", {
 	typescript: require('typescript'),
 	declaration: true
 });
+var projectEs6 = ts.createProject("tsconfig.json", {
+  typescript: require('typescript'),
+  declaration: false,
+  module: "es2015"
+});
 
 // https://github.com/ivogabe/gulp-typescript
-gulp.task("default", function () {
-	var tsResult = gulp.src(['!./src/**/d.ts', './src/**/*.ts']).pipe(project());
-	//var tsResult = project.src().pipe(project());
-	return merge([
-		tsResult.dts.pipe(gulp.dest("dist")),
-		tsResult.js.pipe(gulp.dest("dist"))
-	]);
+gulp.task("es5", function () {
+  var tsResult = gulp.src(['!./src/**/d.ts', './src/**/*.ts']).pipe(project());
+  return merge([
+    tsResult.dts.pipe(gulp.dest("dist")),
+    tsResult.js.pipe(gulp.dest("dist"))
+  ]);
 });
+
+gulp.task("es6", function () {
+  var tsResult = gulp.src(['!./src/**/d.ts', './src/**/*.ts']).pipe(projectEs6());
+  return tsResult.js
+      .pipe(rename(function (path) {
+        //console.log("File Path: " + JSON.stringify(path));
+        if (path.extname === ".js") {
+          path.extname = ".mjs" // convert all .js files for node .mjs
+        }
+      }))
+      .pipe(gulp.dest("dist"))
+});
+
+gulp.task("default", ["es5", "es6"]);
